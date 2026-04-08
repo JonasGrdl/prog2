bfae4a29bc732c2de78d49b36695f2dfb65c1a56
JonasGrdl <Jonas.Groedl@student.htw-berlin.de>
2026-04-08 16:40:57 +0200

    Commit Test

---
+++ app/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        printThis("Hello World");
+    }
+    public static printThis(String text){
+        System.out.println(text);
+    }
+}
